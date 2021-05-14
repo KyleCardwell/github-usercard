ca69b436d6f66d349aa0a241952795ee0ea01e96
Kyle <kyle.e.cardwell@gmail.com>
2021-05-13 20:22:48 -0600

    finished first card with my name

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -7,9 +7,12 @@ import axios from 'axios';
 
 axios.get('https://api.github.com/users/KyleCardwell')
   .then(response => {
-    console.log(response)
+    const newIdCard = idCardMaker(response.data);
+    console.log(newIdCard);
+    const cardsGroup = document.querySelector('.cards')
+    cardsGroup.appendChild(newIdCard);
   })
-  .catch()
+  // .catch()
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -57,6 +60,7 @@ const followersArray = [];
 */
 
 function idCardMaker(gitHubInfo) {
+
   const gitHubAvatar = gitHubInfo.avatar_url;
   const gitHubName = gitHubInfo.name;
   const gitHubLogin = gitHubInfo.login
@@ -78,10 +82,41 @@ function idCardMaker(gitHubInfo) {
   const userFollowing = document.createElement('p');
   const userBio = document.createElement('p');
 
+  cardDiv.classList.add('card');
+  // userImg.classList.add('card img');
+  infoDiv.classList.add('card-info');
+  fullName.classList.add('name');
+  userLogin.classList.add('username');
+
+  userImg.src = gitHubAvatar;
+  fullName.textContent = gitHubName;
+  userLogin.textContent = gitHubLogin;
+  userLoc.textContent = gitHubLoc;
+  userProfile.textContent = 'Profile: ';
+  profileHref.href = gitHubURL;
+  profileHref.textContent = gitHubURL;
+  userFollowers.textContent = `Followers: ${gitHubFollowers}`;
+  userFollowing.textContent = `Following: ${gitHubFollowing}`;
+  userBio.textContent = gitHubBio;
+
+  cardDiv.appendChild(userImg);
+  cardDiv.appendChild(infoDiv);
+  infoDiv.appendChild(fullName);
+  infoDiv.appendChild(userLogin);
+  infoDiv.appendChild(userLoc);
+  infoDiv.appendChild(userProfile);
+  userProfile.appendChild(profileHref);
+  infoDiv.appendChild(userFollowers);
+  infoDiv.appendChild(userFollowing);
+  infoDiv.appendChild(userBio);
+
+  return cardDiv;
+
 }
 
 /*
   List of LS Instructors Github username's:
+    KyleCardwell
     tetondan
     dustinmyers
     justsml
